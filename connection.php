b622478ef93c9f20f40098397cafbee356e8a326
Zakia Afrin Jeme <zjeme@techlaunch.io>
2018-12-20 18:37:19 -0500

    Updated

--- connection.php
+++ connection.php
@@ -1,7 +1,7 @@
 <?php
    $servername ="localhost";
-   $username="root";
-   $password="";
+   $username="phpmyadmin";
+   $password="@1234Nijhum";
    $dbname="stamp_collection";
 
    $conn = mysqli_connect($servername,$username,$password,$dbname);
